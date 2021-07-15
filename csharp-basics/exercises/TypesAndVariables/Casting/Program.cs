d5ef85c796257513fe0595faaba8535b0c314aba
Vita Galindoma <vita.galindoma@gmail.com>
2021-07-15 10:26:32 +0300

    Exercise 3

--- csharp-basics/exercises/TypesAndVariables/Casting/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Casting/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Casting
 {
@@ -24,8 +20,8 @@ namespace Casting
             float e = 5;
 
             //fixme - should be 15 :|
-            //int sum = a + b + c + d + e;
-            //Console.WriteLine(sum);
+            int sum = Convert.ToInt32(a) + b + c + (int)d + (int)e;
+            Console.WriteLine(sum);
         }
 
         static void second()
@@ -37,8 +33,8 @@ namespace Casting
             float e = 5.3f;
 
             //fixme - should be 15.5 :| 
-            //float sum = a + b + c + d + e;
-            //Console.WriteLine(sum);
+            float sum = float.Parse(a) + b + c + (float)d + e;
+            Console.WriteLine(sum);
         }
     }
 }
