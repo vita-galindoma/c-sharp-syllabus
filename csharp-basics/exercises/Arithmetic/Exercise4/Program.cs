eb19991cc3a21a4dc4a94d7896541cd683aca99e
Vita Galindoma <vita.galindoma@gmail.com>
2021-07-16 14:47:11 +0300

    Exercise4

---
+++ csharp-basics/exercises/Arithmetic/Exercise4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product1ToN(10);
+        }
+
+        static void Product1ToN(int n)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            } 
+            Console.WriteLine($"The product of integers from 1 to {n} is {factorial}. ");
+        }
+    }
+}
