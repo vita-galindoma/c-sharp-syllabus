e5470109814ad9cd785f167091c42f6c8a795fbf
Vita Galindoma <vita.galindoma@gmail.com>
2021-07-16 14:46:04 +0300

    Exercise2

---
+++ csharp-basics/exercises/Arithmetic/Exercise2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Transactions;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            CheckOddEven(int.Parse(Console.ReadLine()));
+        }
+
+        static void CheckOddEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+            Console.WriteLine("Bye!");
+        }
+    }
+}
