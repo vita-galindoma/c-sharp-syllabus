118710e8b4c29cfbd953b7102d6d811c9f9e6582
Vita Galindoma <vita.galindoma@gmail.com>
2021-07-20 16:18:57 +0300

    Exercise1

--- csharp-basics/exercises/Loops/Loops/Exercise1/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise1/Program.cs
@@ -6,17 +6,12 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            int i;
-
             Console.WriteLine("The first 10 natural numbers are: ");
-
-            /*
-            todo - write a program in C# to display the first 10 natural numbers
-            for (......) 
+            for (int i = 1; i < 11; i++)
             {
-              Console.WriteLine(i);
+                Console.WriteLine(i);
             }
-            */
+            Console.ReadKey();
         }
     }
 }
