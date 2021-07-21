4488c1202396001c6df2098024d87a1d67f1af3d
Vita Galindoma <vita.galindoma@gmail.com>
2021-07-21 11:19:58 +0300

    Exercise5

---
+++ csharp-basics/exercises/Loops/Loops/Exercise5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalLineLength = 30;
+            Console.WriteLine("Enter first word:");
+            string firstWord = Console.ReadLine();
+            Console.WriteLine("Enter first word:");
+            string secondWord = Console.ReadLine();
+
+            Console.Write(firstWord);
+            for (int i = 0; i < 30 - firstWord.Length - secondWord.Length; i++)
+            {
+                Console.Write(".");
+            }
+            Console.Write(secondWord);
+            Console.ReadKey();
+        }
+    }
+}
