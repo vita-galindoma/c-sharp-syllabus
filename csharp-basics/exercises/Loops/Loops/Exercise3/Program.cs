86141574f42df1c1238ccc5c4a8c5e9d332bcd1c
Vita Galindoma <vita.galindoma@gmail.com>
2021-07-21 10:48:30 +0300

    Exercise3

---
+++ csharp-basics/exercises/Loops/Loops/Exercise3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randomArray = new int[20];
+            Random randNum = new Random();
+            for (int i = 0; i < randomArray.Length; i++)
+            {
+                randomArray[i] = randNum.Next(0, 50);
+                //Console.WriteLine($"{randomArray[i]} ");
+            }
+
+            Console.WriteLine("Which number position out of 20 random numbers (between 0 and 50) would you like to know?");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int index = Array.IndexOf(randomArray, n);
+
+            if (index > 0)
+            {
+                Console.WriteLine($"{n} position is: {index}");
+            }
+            else
+            {
+                Console.WriteLine("Not found.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
