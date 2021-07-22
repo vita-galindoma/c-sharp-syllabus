bf41fa79638ace8ebadb1fdadffe4a56e66dc0c2
Vita Galindoma <vita.galindoma@gmail.com>
2021-07-22 11:05:35 +0300

    Exercise10

---
+++ csharp-basics/exercises/Loops/Loops/Exercise10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercise10
+{
+    class NumberSquare
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Min?");
+            int numberMin = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Max?");
+            int numberMax = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = numberMin; i <= numberMax; i++)
+            {
+                for (int j = i; j <= numberMax; j++)
+                {
+                    Console.Write(j);
+                }
+
+                for (int j = numberMin; j < i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
