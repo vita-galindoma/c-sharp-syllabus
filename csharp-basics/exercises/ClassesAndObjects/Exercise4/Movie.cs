0f2176f93951f9cb756061a90a4b2a7ab22b1a26
Vita Galindoma <vita.galindoma@gmail.com>
2021-07-26 15:47:38 +0300

    Exercise4

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    class Movie
+    {
+        public string title;
+        public string studio;
+        public string rating;
+
+        public Movie(string title, string studio, string rating)
+        {
+            this.title = title;
+            this.studio = studio;
+            this.rating = rating;
+        }
+
+        public Movie(string title, string studio)
+        {
+            this.title = title;
+            this.studio = studio;
+            this.rating = "PG";
+        }
+
+        public static Movie[] GetPG(Movie[] movies)
+        {
+            var result = movies.Where(movie => movie.rating == "PG").ToArray();
+            return result;
+        }
+    }
+}
