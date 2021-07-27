a8763ee8fbc1c7ff0762227bdf64623faaa4c0ec
Vita Galindoma <vita.galindoma@gmail.com>
2021-07-27 14:53:24 +0300

    Exercise9

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(5, 2);
+            Point p2 = new Point(-3, 6);
+            
+            Console.WriteLine("(" + p1.x + ", " + p1.y + ")");
+            Console.WriteLine("(" + p2.x + ", " + p2.y + ")");
+
+            Point.SwapPoints(ref p1, ref p2);
+
+            Console.WriteLine("After Swap:");
+            Console.WriteLine("(" + p1.x + ", " + p1.y + ")");
+            Console.WriteLine("(" + p2.x + ", " + p2.y + ")");
+
+            Console.ReadKey();
+        }
+    }
+}
