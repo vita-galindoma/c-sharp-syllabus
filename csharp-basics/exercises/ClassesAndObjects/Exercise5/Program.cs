e6c7562297da9fdf1780807f146fbcb33bc22d85
Vita Galindoma <vita.galindoma@gmail.com>
2021-07-27 10:08:04 +0300

    Exercise5

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = new Date(10, 10, 2020);
+            date.Year = 2021;
+            date.Month = 8;
+            date.Day = 12;
+
+            date.DisplayDate();
+            Console.ReadKey();
+        }
+    }
+}
