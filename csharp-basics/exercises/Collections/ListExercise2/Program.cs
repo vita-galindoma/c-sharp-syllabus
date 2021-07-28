a2de57488bc99da6a8f0dc48257d05c732bc384c
Vita Galindoma <vita.galindoma@gmail.com>
2021-07-28 12:11:12 +0300

    ListExercise2

--- csharp-basics/exercises/Collections/ListExercise2/Program.cs
+++ csharp-basics/exercises/Collections/ListExercise2/Program.cs
@@ -19,13 +19,12 @@ namespace ListExercise2
             colors.Add("White");
             colors.Add("Black");
 
-            /*
-            fixme
-            for (...) 
+            foreach (var element in colors)
             {
-              Console.WriteLine(element);
+                Console.WriteLine(element);
             }
-            */
+
+            Console.ReadKey();
         }
     }
 }
