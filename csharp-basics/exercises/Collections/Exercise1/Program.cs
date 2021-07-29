c4fb7635b632c2a10cdb683190f6e54ae5bf1b8e
Vita Galindoma <vita.galindoma@gmail.com>
2021-07-29 09:50:16 +0300

    Exercise1

--- csharp-basics/exercises/Collections/Exercise1/Program.cs
+++ csharp-basics/exercises/Collections/Exercise1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Exercise1
 {
@@ -21,12 +18,39 @@ namespace Exercise1
         private static void Main(string[] args)
         {
             string[] array = { "Audi", "BMW", "Honda", "Mercedes", "VolksWagen", "Mercedes", "Tesla" };
+            Console.WriteLine("Array:");
+            Console.WriteLine(string.Join(", ", array));
+            Console.WriteLine();
 
             //todo - replace array with an List and print out the results
+            var list = new List<string> {"Audi", "BMW", "Honda", "Mercedes", "VolksWagen", "Mercedes", "Tesla"};
+            Console.WriteLine("List:");
+            Console.WriteLine(string.Join(", ", list));
+            Console.WriteLine();
 
             //todo - replace array with a HashSet and print out the results
+            var hashset = new HashSet<string> { "Audi", "BMW", "Honda", "Mercedes", "VolksWagen", "Mercedes", "Tesla" };
+            Console.WriteLine("Hashset:");
+            Console.WriteLine(string.Join(", ", hashset));
+            Console.WriteLine();
 
             //todo - replace array with a Dictionary (use brand as key and origination as value) and print out the results
+            var dictionary = new Dictionary<string, string>();
+            dictionary.Add("Audi", "Germany");
+            dictionary.Add("BMW", "Germany");
+            dictionary.Add("Honda", "Japan");
+            dictionary.Add("Mercedes", "Germany");
+            dictionary.Add("VolksWagen", "Germany");
+            dictionary.Add("Tesla", "USA");
+
+            Console.WriteLine("Dictionary:");
+            
+            foreach (var element in dictionary)
+            {
+                Console.WriteLine($"{element.Key} => {element.Value}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
