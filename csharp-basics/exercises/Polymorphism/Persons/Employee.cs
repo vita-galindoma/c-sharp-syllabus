f4e66800f24b3320b90f3a60472012906260d27a
Vita Galindoma <vita.galindoma@gmail.com>
2021-08-11 15:02:24 +0300

    Persons

---
+++ csharp-basics/exercises/Polymorphism/Persons/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Persons
+{
+    public class Employee: Person
+    {
+        private string _jobTitle;
+        public string JobTitle 
+        {
+            get => _jobTitle;
+            set => _jobTitle = value;
+        }
+
+        public Employee(string firstName, string lastName, string address, int id)
+        {
+        }
+
+        public Employee(string firstName, string lastName, string address, int id, string jobTitle) : base(firstName, lastName, address, id)
+        {
+            _jobTitle = jobTitle;
+        }
+        public override void Display()
+        {
+            Console.WriteLine($"First name: {FirstName} \nLast name: {LastName} \nAddress: {Address} \nID: {Id} \nJob title: {_jobTitle}\n");
+        }
+    }
+}
---
+++ csharp-basics/exercises/Polymorphism/Persons/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Persons
+{
+    class Student: Person
+    {
+        private double _gpa;
+        public double Gpa 
+        {
+            get => _gpa;
+            set => _gpa = value;
+        }
+        public Student(string firstName, string lastName, string address, int id)
+        {
+
+        }
+
+        public Student(string firstName, string lastName, string address, int id, double gpa) : base(firstName, lastName, address, id)
+        {
+            _gpa = gpa;
+        }
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine($"GPA: {_gpa}\n");
+        }
+    }
+}
