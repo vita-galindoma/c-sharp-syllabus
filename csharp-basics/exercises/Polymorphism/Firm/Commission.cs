695aa121d21f2682ce704cf793e9589106bf9f26
Vita Galindoma <vita.galindoma@gmail.com>
2021-08-12 11:12:50 +0300

    Firm

---
+++ csharp-basics/exercises/Polymorphism/Firm/Commission.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Firm
+{
+    class Commission : Hourly
+    {
+        private double _totalSales;
+        private double _commissionRate;
+        public Commission(string eName, string eAddress, string ePhone, string socSecNumber, double rate, double commissionRate) : base(eName, eAddress, ePhone, socSecNumber, rate)
+        {
+            _commissionRate = commissionRate;
+            _totalSales = 0;
+        }
+
+        public void addSales(double totalSales)
+        {
+            _totalSales+=totalSales;
+        }
+
+        public override double Pay()
+        {
+            var payment = base.Pay() + (_totalSales * _commissionRate);
+            _totalSales = 0;
+            return payment;
+        }
+
+        public override string ToString()
+        {
+            var result = base.ToString();
+            result += "\nTotal Sales: " + _totalSales;
+            return result;
+        }
+
+    }
+}
--- csharp-basics/exercises/Polymorphism/Firm/Hourly.cs
+++ csharp-basics/exercises/Polymorphism/Firm/Hourly.cs
@@ -25,7 +25,7 @@ namespace Firm
         // Computes and returns the pay for this hourly employee.
         //-----------------------------------------------------------------
         public override double Pay() 
-
+        {
             var payment = payRate * _hoursWorked;
             _hoursWorked = 0;
             return payment;
@@ -40,5 +40,6 @@ namespace Firm
             result += "\nCurrent hours: " + _hoursWorked;
             return result;
         }
+
     }
 }
