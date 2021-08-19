95715f42e7d087725faf9be2b22cb29956b1a1ea
Vita Galindoma <vita.galindoma@gmail.com>
2021-08-19 17:50:28 +0300

    Exercise1Tests

--- csharp-basics/exercises/Arithmetic/Exercise1/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise1/Program.cs
@@ -7,12 +7,10 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Input first integer: ");
-            int x = int.Parse(Console.ReadLine());
-            Console.WriteLine("Input second integer: ");
-            int y = int.Parse(Console.ReadLine());
-            bool test = (x == 15) || (y == 15) || (x + y == 15) || Math.Abs(x + y) == 15;
+            bool test = Exercise1Extension.IsFifteen(1, 15);
             Console.WriteLine($"One of the integers or their sum is 15: {test}");
+
+            Console.ReadKey();
         }
     }
 }
