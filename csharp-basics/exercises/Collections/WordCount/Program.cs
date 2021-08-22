1bfd91568b149ccb0aa7c9425e1d4aef9bda4be8
Vita Galindoma <vita.galindoma@gmail.com>
2021-08-22 13:20:27 +0300

    WordCountTests

--- csharp-basics/exercises/Collections/WordCount/Program.cs
+++ csharp-basics/exercises/Collections/WordCount/Program.cs
@@ -10,19 +10,9 @@ namespace WordCount
         {
             string text = File.ReadAllText("../../lear.txt");
 
-            int countLines = text.Split('\n').Length;
-            Console.WriteLine($"Lines = {countLines}");
-
-            string[] wordsArray = text.Replace("'", " ").Replace("  ", " ").Split(' ', '\n');
-            int countWords = 0;
-            foreach (var words in wordsArray)
-            {
-                countWords++;
-            }
-            Console.WriteLine($"Words = {countWords}");
-            
-            int countChar = text.Length;
-            Console.WriteLine($"Chars = {countChar}");
+            Console.WriteLine($"Lines = {WordCountExt.CalculateLineCount(text)}");
+            Console.WriteLine($"Words = {WordCountExt.CalculateWordCount(text)}");
+            Console.WriteLine($"Chars = {WordCountExt.CalculateCharCount(text)}");
 
             Console.ReadKey();
         }
---
+++ csharp-basics/exercises/Collections/WordCount/WordCountExt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCount
+{
+    public class WordCountExt
+    {
+
+        public static int CalculateLineCount(string text)
+        {
+            int countLines = text.Split('\n').Length;
+            return countLines;
+        }
+        public static int CalculateWordCount(string text)
+        {
+            string[] wordsArray = text.Replace("'", " ").Replace("  ", " ").Split(' ', '\n');
+            int countWords = 0;
+            foreach (var words in wordsArray)
+            {
+                countWords++;
+            }
+            return countWords;
+        }
+
+        public static int CalculateCharCount(string text)
+        {
+            int countChar = text.Length;
+            return countChar;
+        }
+
+    }
+}
